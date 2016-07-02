1617ac2e338b756f7541108be76c2b6452141898
Ahmad Ali <ahmad.ali@square63.com>
2016-07-02 17:18:46 +0500

    Home: added top, latest and featured sections

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   before_action :authenticate_user!
 
   def home
+    @latest_movies = Movie.get_latest_movies
+    @featured_movies = Movie.get_featured_movies
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,12 +1,17 @@
 class Movie < ActiveRecord::Base
 
   paginates_per 10
+  LATEST_MOVIES_LIMIT = 4
+  FEATURED_MOVIES_LIMIT = 4
   GENRE = ["Action", "Horror", "Romance", "Documentary", "Biography", "Comedy", "Crime", "Drama", "Romance", "War"]
 
   has_many :images, as: :imageable
   accepts_nested_attributes_for :images, allow_destroy: true
   has_many :movie_casts
   has_many :actors, through: :movie_casts
+
+  scope :latest, -> { order(released_date: :desc) }
+  scope :featured, -> { where(is_featured: true) }
 
   validates :name, presence: true, length: { maximum: 60 }
   validates :released_date, presence: true
@@ -15,6 +20,20 @@ class Movie < ActiveRecord::Base
 
   def all_movie_actors
     return self.actors.collect(&:name).join(', ')
+  end
+
+  def self.get_latest_movies
+    self.latest.limit(LATEST_MOVIES_LIMIT);
+  end
+
+  def self.get_featured_movies
+    self.featured.limit(FEATURED_MOVIES_LIMIT)
+  end
+
+  def self.get_movies(movie_params)
+    return self.featured if movie_params[:featured].present?
+    return self.latest if movie_params[:latest].present?
+    return self.all
   end
 
 end
