d4b70f13e7c834b76296c3aa82a5bbb7a3c86625
Ahmad Ali <ahmad.ali@square63.com>
2016-07-19 11:02:34 +0500

    Home: remove user authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,5 @@
 class PagesController < ApplicationController
 
-  before_action :authenticate_user!
-
   def home
     @latest_movies = Movie.get_latest_movies
     @featured_movies = Movie.get_featured_movies
