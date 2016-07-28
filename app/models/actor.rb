13d311f16e6b2ae0b4d71df236ee6e7743306399
Ahmad Ali <ahmad.ali@square63.com>
2016-07-28 22:27:16 +0500

    Actor: added model level validation to the gender

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,7 +5,7 @@ class Actor < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 60 }
   validates :country, length: { maximum: 20 }
-  validates :gender, length: { maximum: 10 }
+  validates :gender, length: { maximum: 10 }, inclusion: { in: User::GENDERS, if: 'gender.present?' }
   validates :date_of_birth, length: { maximum: 20 }
 
 end
