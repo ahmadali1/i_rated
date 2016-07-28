95ba631db951c11d56fd37a26115295b74c59f50
Ahmad Ali <ahmad.ali@square63.com>
2016-07-28 09:10:36 +0500

    User: refactored code of controller and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
-    @favourite_movies = @user.favourite_movies.page(params[:page]).per Movie::FAVOURITE_PER
+    @favourite_movies = current_user.favourite_movies.page(params[:page]).per Movie::FAVOURITE_PER
   end
 
 end
