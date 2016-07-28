335b2d4377c70be148766f266971551eb74e9ca2
Ahmad Ali <ahmad.ali@square63.com>
2016-07-28 22:09:27 +0500

    Api: refactored code of controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -4,39 +4,28 @@ module Api
       before_action :validate_date_range, only: :index
       before_action :set_movie, only: :show
       before_action :authenticate_request
+      respond_to :json
 
       def index
         @movies = Movie.search_movie params
-        respond_to do |format|
-          format.json { render json: {movies: @movies}, status: 200 }
-        end
+        render json: { movies: @movies }, status: 200
       end
 
       def show
-        status = if @movie.present?
-          200
-        else
-          404
-        end
-
-        respond_to do |format|
-          format.json { render json: { movie: @movie.movie_hash(request.env['HTTP_HOST']) }, status: status }
-        end
-
+        render json: { movie: @movie.movie_hash(request.env['HTTP_HOST']) }, status: 200
       end
 
       private
         def validate_date_range
           message = valid_date_range(params)
           if message
-            respond_to do |format|
-              format.json { render json: { Error: message }, status: 400 }
-            end
+            render json: { Error: message }, status: 400
           end
         end
 
         def set_movie
           @movie = Movie.includes(:reviews, :ratings).find_by_id(params[:id])
+          render json: {}, status: 404 if @movie.blank?
         end
 
         def authenticate_request
